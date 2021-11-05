6082f8d53afa30dd8c5ffae4f6a2618175000e28
hurrykane0511 <86058971+hurrykane0511@users.noreply.github.com>
2021-11-05 11:25:49 +0700

    update

--- signup.php
+++ signup.php
@@ -8,8 +8,16 @@
             <div class="visual-login">
                 <?php include './Template/header.php' ?>
                 <div class="form-container">
-                    <h2 class="form-title">Login</h2>
+                    <h2 class="form-title">Create Account</h2>
                     <form action="./index.php" class="login-form">
+                        <div class="input-group">
+                            <label for="firstname">First name</label>
+                            <input type="text" name="firstname" id="firstname">
+                        </div>
+                        <div class="input-group">
+                            <label for="lastname">Last name</label>
+                            <input type="text" name="lastname" id="lastname">
+                        </div>
                         <div class="input-group">
                             <label for="email">Email</label>
                             <input type="text" name="email" id="email">
@@ -19,9 +27,8 @@
                             <input type="password" name="password" id="pass">
                         </div>
                         <div class="input-group">
-                            <input type="submit" name="signin" class="login-btn" value="Sign In">
+                            <input type="submit" name="signup" class="login-btn" value="Sign Up">
                         </div>
-                        <a href="signup.php" class="create-account">Create account</a>
                     </form>
                 </div>
             </div>
