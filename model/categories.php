3e15995c033c18da38e22637d628541a3b14dc09
Taiemcute <85283737+Taiem15032002@users.noreply.github.com>
2021-11-24 13:09:10 +0700

    Update categories.php

--- model/categories.php
+++ model/categories.php
@@ -1,8 +1,9 @@
 <?php
-class categories extends Db{
+class categories extends Db
+{
     function getAllType()
     {
-        $sql = self::$connection->prepare("SELECT * FROM tbl_type");
+        $sql = self::$connection->prepare("SELECT * FROM `tbl_type`");
         $sql->execute();
         $Types = array();
         $Types = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
@@ -13,8 +14,7 @@ class categories extends Db{
         $sql = self::$connection->prepare("SELECT * FROM tbl_range");
         $sql->execute();
         $Ranges = array();
-        $Ranges= $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        $Ranges = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
         return  $Ranges;
     }
 }
-?>
